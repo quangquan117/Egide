2c0a4ceb160a8c1f654f8cfb748e5b7c60be3c76
quangquan <leo.langlois2@gmail.com>
2024-03-27 16:31:55 +0100

    Fix API endpoint handling in api.php

--- API/api.php
+++ API/api.php
@@ -1,8 +1,10 @@
 <?php
     include_once "user.php";
-    
+
     if (strpos($_SERVER["REQUEST_URI"], "/new_user") !== false){
+        
         $data = json_decode(file_get_contents("php://input"), true);
+
         if (isset($data["username"]) && isset($data["email"]) && isset($data["password"])) {
             echo newUser($data["username"], $data["email"], $data["password"]);
             http_response_code(200);
@@ -11,7 +13,9 @@
             http_response_code(404);
         }
     } else if (strpos($_SERVER["REQUEST_URI"], "/conn_user") !== false) {
+
         $data = json_decode(file_get_contents("php://input"), true);
+        
         if (isset($data["username"]) && isset($data["password"])) {
             echo connUser($data["username"], $data["password"]);
             http_response_code(200);
