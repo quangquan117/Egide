cb9faef957f0618b1cff787fe0dc4dac0d1ca192
quangquan <leo.langlois2@gmail.com>
2024-04-05 16:47:13 +0200

    Add margin to navigation and update grid layout in store section

---
+++ store.php
@@ -0,0 +1,31 @@
+<?php
+  require_once __DIR__.'/src/template/head.inc.php';
+  require_once __DIR__.'/src/template/header.inc.php';
+  require_once __DIR__.'/src/template/nav.inc.php';
+?>
+        <section id="store">
+            <ul>
+                <li><img src="https://placehold.jp/150x150.png" alt="placehold"></li>
+                <li><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident molestiae esse incidunt voluptates ut.</p></li>
+            </ul>
+            <ul>
+                <li><img src="https://placehold.jp/150x150.png" alt="placehold"></li>
+                <li><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident molestiae esse incidunt voluptates ut.</p></li>
+            </ul>
+            <ul>
+                <li><img src="https://placehold.jp/150x150.png" alt="placehold"></li>
+                <li><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident molestiae esse incidunt voluptates ut.</p></li>
+            </ul>
+            <ul>
+                <li><img src="https://placehold.jp/150x150.png" alt="placehold"></li>
+                <li><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident molestiae esse incidunt voluptates ut.</p></li>
+            </ul>
+            <ul>
+                <li><img src="https://placehold.jp/150x150.png" alt="placehold"></li>
+                <li><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident molestiae esse incidunt voluptates ut.</p></li>
+            </ul>
+            <ul>
+                <li><img src="https://placehold.jp/150x150.png" alt="placehold"></li>
+                <li><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident molestiae esse incidunt voluptates ut.</p></li>
+            </ul>
+        </section>
